678fc1b3aaa9402a17472e6c095ef1be2e471eda
ArtViso <off.club1515@gmail.com>
2021-02-28 19:00:26 +0200

    last

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const randcolor = document.querySelector(".randcolor");
+const randcolor = document.querySelector(".randcolor"),
+  colors = document.querySelector(".colors");
 
 randcolor.addEventListener("click", randcolors);
 
 function randcolors() {
-  let color = "#";
-  let symbol = "0123456789ABCDEF";
+  let color = "0123456789ABCDEF";
+  let a = "#";
   for (let i = 0; i < 6; i++) {
-    color += symbol[Math.floor(Math.random() * 16)];
+    a += color[Math.floor(Math.random() * 16)];
   }
-  document.body.style.background = color;
+  document.body.style.backgroundColor = a;
+  colors.innerHTML = a;
 }
